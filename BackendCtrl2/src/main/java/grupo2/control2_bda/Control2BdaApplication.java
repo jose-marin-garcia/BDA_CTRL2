dcf830065b9a28ac34d97fc2fb9a83ffe1bfb52d
jose-marin-garcia <jose.marin.g@usach.cl>
2024-11-06 11:55:35 -0300

    Add backend and frontend project

---
+++ BackendCtrl2/src/main/java/grupo2/control2_bda/Control2BdaApplication.java
@@ -0,0 +1,13 @@
+package grupo2.control2_bda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Control2BdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Control2BdaApplication.class, args);
+	}
+
+}
---
+++ BackendCtrl2/src/test/java/grupo2/control2_bda/Control2BdaApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.control2_bda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control2BdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
